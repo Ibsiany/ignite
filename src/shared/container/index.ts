68d59fc9d92af2041f5765e4fb60a4a6e4f2f5bc
ibsiany <ibsianyd@gmail.com>
2021-04-29 09:15:00 -0300

    update list cars

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -6,7 +6,7 @@ import { SpecificationsRepository } from '@modules/cars/infra/typeorm/repositori
 import {ICategoriesRepository} from '@modules/cars/repositories/ICategoriesRepository';
 import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationsRepository';
 import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
-import { CarsRepository } from '@modules/cars/repositories/CarsRepository';
+import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository';
 
 container.registerSingleton<ICategoriesRepository>(
     "CategoriesRepository",
