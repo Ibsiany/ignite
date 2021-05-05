6af9888c122e4d8ec5112cd5de19e01d0ed72af3
ibsiany <ibsianyd@gmail.com>
2021-05-05 09:15:59 -0300

    rental

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,15 +1,21 @@
 import {Router} from 'express'
+import multer from 'multer'
+import uploadConfig from '../../../../config/upload'
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController'
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated'
 import { ensureAdmin } from '../middlewares/ensureAdmin'
 import { ListAvailableCarsController } from '@modules/cars/useCases/listAvailableCars/ListAvailableCarsController'
+import { UploadCarImagesController } from '@modules/cars/useCases/uploadCarImage/UploadCarImagesController'
 import { CreateCarSpecificationController } from '@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController'
 
 const carsRoutes = Router()
 
+
 const createCarController = new CreateCarController()
 const listAvailableCarsController = new ListAvailableCarsController()
 const createCarSpecificationController = new CreateCarSpecificationController();
+const uploadCarImageController = new UploadCarImagesController();
+const upload = multer(uploadConfig.upload("./tmp/avatar"));
 
 carsRoutes.post("/", 
     ensureAuthenticated, 
@@ -19,9 +25,15 @@ carsRoutes.post("/",
 
 carsRoutes.get("/available", listAvailableCarsController.handle)
 
-carsRoutes.post("specifications/:id", 
+carsRoutes.post("/specifications/:id", 
     ensureAuthenticated, 
     ensureAdmin,
     createCarSpecificationController.handle)
+
+carsRoutes.post("/images/:id", 
+    ensureAuthenticated, 
+    ensureAdmin,
+    upload.array("images"),
+    uploadCarImageController.handle)
 
 export {carsRoutes}
--- src/shared/infra/typeorm/migrations/1618768015548-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1618768015548-CreateUsers.ts
@@ -9,7 +9,8 @@ export class CreateUsers1618768015548 implements MigrationInterface {
                 columns: [
                     {
                         name: "id",
-                        type: "uuid"
+                        type: "uuid",
+                        isPrimary: true
                     },
                     {
                         name: "name",
