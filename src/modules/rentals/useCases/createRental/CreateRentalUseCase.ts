8abb8e0b6a93c7c36f8f3995efdb50224f7d2e85
ibsiany <ibsianyd@gmail.com>
2021-05-06 09:25:04 -0300

    date provider

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,5 +1,7 @@
+
 import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { AppError } from "@shared/errors/AppError";
 
 interface IRequest {
@@ -11,10 +13,12 @@ interface IRequest {
 class CreateRentalUseCase {
 
     constructor(
-        private rentalsRepository: IRentalsRepository
+        private rentalsRepository: IRentalsRepository,
+        private dateProvider: IDateProvider
     ){}
 
     async execute({user_id, car_id, expected_return_date}:IRequest): Promise<Rental> {
+        const minimumHour = 24;
         //- Não deve ser possível cadastrar um novo aluguel caso já exista um aberto para o mesmo carro.
         const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(car_id);
         if(carUnavailable){
@@ -28,8 +32,12 @@ class CreateRentalUseCase {
         }
 
         //- O aluguel deve ter duração mínima de 24 horas.
+        const dateNow = this.dateProvider.dateNow()
+        const compare = this.dateProvider.compareInHours(dateNow, expected_return_date)
 
-
+        if(compare < minimumHour){
+            throw new AppError("Invalid return time!")
+        }
 
         const rental = await this.rentalsRepository.create({
             user_id,
