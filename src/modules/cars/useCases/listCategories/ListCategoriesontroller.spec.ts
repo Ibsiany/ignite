eb7a16e6ada0caaabb655fac479462433b6730ce
ibsiany <ibsianyd@gmail.com>
2021-05-10 17:37:34 -0300

    teste create/list category

--- src/modules/cars/useCases/listCategories/ListCategoriesontroller.spec.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesontroller.spec.ts
@@ -22,12 +22,12 @@ describe('List categories', () => {
             `
         );
     })
-
+    
     afterAll(async () => {
         await connection.dropDatabase();
         await connection.close()
     })
-
+    
     it("should be able to list all categories", async () => {
         const responseToken = await request(app).post("/sessions")
             .send({
@@ -35,23 +35,22 @@ describe('List categories', () => {
                 password: "admin",
             })
 
-        const {token} = responseToken.body;
+        const {refresh_token} = responseToken.body;
 
         await request(app)
         .post("/categories")
-        .send({ 
-            name: "Category Supertest",
-            description: "Category Supertest"
-        }).set({
-            Authorization: `Bearer ${token}`
+        .send({
+          name: "Category Supertest",
+          description: "Category Supertest",
         })
+        .set({
+          Authorization: `Bearer ${refresh_token}`,
+        });
 
         const response = await request(app).get("/categories");
 
-        console.log(response.body)
-
         expect(response.status).toBe(200);
-        expect(response.body.lenght).toBe(1);
+        expect(response.body.length).toBe(1);
         expect(response.body[0]).toHaveProperty("id");
         expect(response.body[0].name).toEqual("Category Supertest");
 
