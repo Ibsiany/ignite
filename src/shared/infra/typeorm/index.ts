62c36a8be357d39ff7e437655445c8ed3c40140c
ibsiany <ibsianyd@gmail.com>
2021-06-29 08:06:08 -0300

    ajuste database

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,10 +1,9 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-export default async(host = "database_ignite"): Promise<Connection> => {
+export default async(): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === 'test' ? 'localhost' :host, //database_ignite Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
       database: process.env.NODE_ENV === 'test'
         ? "rentx_test" 
         : defaultOptions.database,
