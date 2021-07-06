14ed32a3b5ea8cc068083ff081bd603a7e0d229a
ibsiany <ibsianyd@gmail.com>
2021-07-06 08:39:42 -0300

    cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -3,6 +3,7 @@ import "dotenv/config";
 import express, { NextFunction, Request, Response } from 'express';
 import "express-async-errors";
 import swaggerUi from 'swagger-ui-express' 
+import cors from 'cors';
 
 import "../typeorm"
 import "../../../shared/container"
@@ -20,6 +21,8 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use("avatar", express.static(`${upload.tmpFolder}/avatar`))
 app.use("cars", express.static(`${upload.tmpFolder}/cars`))
+
+app.use(cors())
 
 app.use(router)
 
